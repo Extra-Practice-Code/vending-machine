89c1faabedbac9bbf4ed647fde8965116ea2b4a9
Nuel van Gelder de Neufville <github@neufv.systems>
2019-10-16 11:16:03 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+/* global Vue, fetch */
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    entries: [],
+    singleProduct: false,
+    host: false
+  },
+  mounted () {
+    if (window.location.hash && window.location.hash.slice(1)) {
+      this.singleProduct = window.location.hash.slice(1)
+    }
+
+    this.host = window.location.hostname.split('.')[0]
+  }
+})
+
+function refresh () {
+  fetch('https://marketplace.e-rob.nl/api/collections/get/uploads?token=account-311b5b245eceac5ab10804a2c0417d&sort[_created]=-1')
+    .then(collection => collection.json())
+    .then(collection => {
+      if (app.singleProduct) {
+        for (let entry of collection.entries) {
+          if (entry._id === app.singleProduct) {
+            app.entries = [entry]
+          }
+        }
+      } else {
+        app.entries = collection.entries
+      }
+    })
+}
+
+setInterval(refresh, 5000)
+refresh()
